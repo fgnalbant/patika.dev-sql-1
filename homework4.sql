242a157aebf3a9ce1424b588ea8ea1e367767f91
Gizem Nalbant Arslan <57711618+fgnalbant@users.noreply.github.com>
2022-06-11 01:09:02 +0300

    Create homework4.sql

---
+++ homework4.sql
@@ -0,0 +1,11 @@
+1.SELECT DISTINCT replacement_cost FROM;
+
+2.SELECT COUNT(DISTINCT replacement_cost) FROM;
+
+3.SELECT COUNT(title) FROM film
+  WHERE title = ‘T%’ AND rating=’G’;
+  
+4.SELECT COUNT(country) FROM WHERE length IN 5;
+
+5.SELECT COUNT(city) FROM city 
+  WHERE city =’R%’ OR city = ‘%r’;
